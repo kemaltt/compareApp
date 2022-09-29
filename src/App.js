dfa6bcfcca33ecef26236287cd7de796fcd0eddb
Kemal <ttnckemal@gmail.com>
2022-09-29 12:08:32 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Contact from './pages/Contact';
 import AboutUs from './pages/AboutUs';
 import ProductDetail from './components/ProductDetail';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -20,8 +21,8 @@ function App() {
           <Route path='/contact' element={<Contact />} />
           <Route path='/aboutus' element={<AboutUs />} />
           <Route path='/productdetail/:id' element={<ProductDetail data={data} />} />
-
         </Routes>
+        <Footer />
       </BrowserRouter>
 
 
