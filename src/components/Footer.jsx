ac40c2ecfcaa2e98fb45d69ddd483837a4ca8764
Kemal <ttnckemal@gmail.com>
2022-10-13 18:33:59 +0200

    footer new Date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer_container">
-      <p> &copy;2022 The Compare App </p>
+      <p> &copy;{new Date().getFullYear()} The Compare App </p>
       <h4>Created with love by Kemal</h4>
     </div>
   )
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function AboutUs() {
-  return <div>AboutUs</div>
+  return <div style={{ height: '80vh' }}>AboutUs</div>
 }
